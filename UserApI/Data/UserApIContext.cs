825fdac6b1127e2486406721813cd9deac1eded4
Nagy János <nagy@zold.lan>
2024-05-13 19:48:32 +0200

    Add project files.

---
+++ UserApI/Data/UserApIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UserApI.Model;
+
+namespace UserApI.Data
+{
+    public class UserApIContext : DbContext
+    {
+        public UserApIContext (DbContextOptions<UserApIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserApI.Model.User> User { get; set; } = default!;
+    }
+}
---
+++ UserApI/Model/User.cs
@@ -0,0 +1,13 @@
+﻿namespace UserApI.Model
+{
+    public class User
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public User()
+        {
+        }
+    }
+}
